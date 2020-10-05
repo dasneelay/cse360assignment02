2c3923600236b18e6f16fe267fe87ba8100c1aef
dasne <dasne@DESKTOP-URFAKBT.lan>
2020-10-05 12:29:12 -0700

    added documentation

--- cse360/src/cse360assignment02/AddingMachine.java
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -2,7 +2,14 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
-
+	/**
+	 * AddingMachine() method sets total to 0
+	 * getTotal() returns total value
+	 * add() adds parameter value to total
+	 * subtract() subtracts parameter value from total
+	 * toString() returns history of transactions
+	 * clear() clears memory
+	 */
 	public AddingMachine() {
 		total = 0; // not needed - included for clarity
 	}
